5f07bd63b74ad344d0b14b90c34085ef56761c9a
mgenece <customerservice@speakwide.com>
2026-04-29 06:23:26 -0400

    Integrate Google Analytics 4 in _document.tsx
    Added Google Analytics 4 scripts for tracking.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,52 @@ import {
   documentGetInitialProps,
 } from '@mui/material-nextjs/v15-pagesRouter';
 import { DocumentContext, DocumentProps, Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+
+interface MyDocumentProps extends DocumentProps, DocumentHeadTagsProps {}
+
+export default function MyDocument(props: MyDocumentProps) {
+  return (
+    <Html lang='en'>
+      <Head>
+        <DocumentHeadTags {...props} />
+        {/* PWA primary color */}
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <link
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        {/* Google Analytics 4 */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZMGPKCG72S"
+        />
+        <Script strategy="afterInteractive" id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZMGPKCG72S');
+          `}
+        </Script>
+      </body>
+    </Html>
+  );
+}
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const finalProps = await documentGetInitialProps(ctx);
+  return finalProps;
+};import {
+  DocumentHeadTags,
+  DocumentHeadTagsProps,
+  documentGetInitialProps,
+} from '@mui/material-nextjs/v15-pagesRouter';
+import { DocumentContext, DocumentProps, Head, Html, Main, NextScript } from 'next/document';
 
 interface MyDocumentProps extends DocumentProps, DocumentHeadTagsProps {}
 
